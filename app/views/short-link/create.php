40e307ff34f013d74aa3e3bc0d51d598fb396f69
Kirill Chirkov <ouroboros-44@yandex.ru>
2026-02-19 22:57:06 +0300

    Сделал фронт

--- app/views/short-link/create.php
+++ app/views/short-link/create.php
@@ -1,7 +1,9 @@
 <?php
 
 use yii\base\InvalidConfigException;
+use yii\bootstrap5\BootstrapPluginAsset;
 use yii\helpers\Html;
+use yii\web\JqueryAsset;
 use yii\widgets\ActiveForm;
 
 /** @var yii\web\View $this */
@@ -11,26 +13,72 @@ use yii\widgets\ActiveForm;
 try {
     $this->registerJsFile(
             '@web/js/shortLinkCreate.js',
-            ['depends' => [\yii\web\JqueryAsset::class]]
+            ['depends' =>
+                    [
+                            JqueryAsset::class,
+                            BootstrapPluginAsset::class,
+                    ]
+            ]
+    );
+
+    $this->registerJsFile(
+            'https://cdn.jsdelivr.net/npm/davidshimjs-qrcodejs@0.0.2/qrcode.min.js',
+            ['depends' => [JqueryAsset::class]]
     );
 } catch (InvalidConfigException $e) {
 
 }
 ?>
 
-<div class="short-link-index">
+<div class="short-link-index d-flex align-items-center justify-content-center min-vh-100">
 
-    <?php $form = ActiveForm::begin([
-            'id' => 'short-link-create-form',
-            'action' => ['short-link/create-ajax'],
-    ]); ?>
+    <div class="w-50">
+        <?php $form = ActiveForm::begin([
+                'id' => 'short-link-create-form',
+                'action' => ['short-link/create-ajax'],
+        ]); ?>
+        <div class="row">
 
-    <?= $form->field($model, 'url') ?>
+            <div class="col-9">
+                <?= $form->field($model, 'url')
+                        ->textInput([
+                                'placeholder' => 'Введите ссылку, которую хотите сократить',
+                                'class' => 'form-control form-control-lg',
+                        ])->label(false) ?>
+            </div>
 
-    <div class="form-group">
-        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+            <div class="col-1">
+                <div class="form-group">
+                    <?= Html::submitButton('Сократить', ['class' => 'btn btn-primary btn-lg']) ?>
+                </div>
+            </div>
+
+        </div>
+        <?php ActiveForm::end(); ?>
+
+        <div class="accordion d-none" id="accordion-short-link">
+            <div class="accordion-item">
+                <div id="collapse-short-link" class="accordion-collapse collapse" aria-labelledby="heading-short-link"
+                     data-bs-parent="#accordion-short-link">
+                    <div class="accordion-body">
+                        <div class="row">
+                            <div class="col-9">
+                                <p type="text" id="short-url" class="text-center fs-3">&nbsp;</p>
+                            </div>
+                            <div class="col-1">
+                                <button id="copy-short-link" class="btn btn-primary btn-lg" disabled>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-copy" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"/>
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+
+                        <div id="qr-wrapper" class="d-flex justify-content-center align-items-center my-4">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
-
-    <?php ActiveForm::end(); ?>
-
-</div>
--- app/web/js/shortLinkCreate.js
+++ app/web/js/shortLinkCreate.js
@@ -1,18 +1,93 @@
 const $form = $('#short-link-create-form');
+const $copyBtn = $('#copy-short-link');
+const $shortUrlP = $('#short-url');
+const $qrWrapper = $('#qr-wrapper');
+const $accordionShortLink = $('#accordion-short-link')
+
+const collapseShortLink = new bootstrap.Collapse($('#collapse-short-link'), {toggle: false})
+let processingRequestFlag = false;
+
 $form.on('beforeSubmit', function (e) {
+    if (processingRequestFlag) {
+        return false;
+    }
+
     let data = $form.serialize();
+
+    processingRequest();
 
     $.ajax({
         url: $form.attr('action'),
         type: 'POST',
         data: data,
         success: function (response) {
+            response = JSON.parse(response);
 
+            $shortUrlP.html(response.data.url);
+            $copyBtn.prop('disabled', false);
+
+            updateQrCode(response.data.url);
+            $accordionShortLink.removeClass('d-none');
+
+            collapseShortLink.show();
+
+            endProcessingRequest();
         },
         error: function (response) {
 
+            endProcessingRequest();
         },
     });
 
     return false;
 });
+
+$copyBtn.on('click', function () {
+    try {
+        // Работает только с HTTPS или на localhost
+        navigator.clipboard.writeText($shortUrlP.text());
+    } catch (e) {
+        // Устаревший метод, но пока поддерживается
+        let range = document.createRange();
+        range.selectNodeContents($shortUrlP[0]);
+
+        let selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+
+        document.execCommand('copy');
+    }
+
+});
+
+function processingRequest() {
+    processingRequestFlag = true;
+
+    $form.find('#createform-url').prop('disabled', true);
+    $form.find('[type="submit"]').prop('disabled', true);
+
+    collapseShortLink.hide();
+    $accordionShortLink.addClass('d-none');
+
+    $shortUrlP.html('');
+    $qrWrapper.html('');
+    $copyBtn.prop('disabled', true);
+}
+
+function endProcessingRequest() {
+    processingRequestFlag = false;
+
+    $form.find('#createform-url').prop('disabled', false);
+    $form.find('[type="submit"]').prop('disabled', false);
+}
+
+function updateQrCode(url) {
+    let qrcode = new QRCode($qrWrapper[0], {
+        text: url,
+        width: 200,
+        height: 200,
+        colorDark : "#000000",
+        colorLight : "#ffffff",
+        correctLevel : QRCode.CorrectLevel.H // High error correction level
+    });
+}
